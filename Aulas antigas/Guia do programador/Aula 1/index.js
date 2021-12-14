d5d51742f3c83e0cf07ca8abc79847047d7b0a3d
Carlos Amorim <carlos.av.amorim@gmail.com>
2021-12-14 14:22:14 -0300

    Alterando porta e testando git

--- Aulas antigas/Guia do programador/Aula 1/index.js
+++ Aulas antigas/Guia do programador/Aula 1/index.js
@@ -6,18 +6,18 @@ app.get('/' , (req, res) => {
     res.send('Ola mundo! Seja bem vindo ao meu app!')
 })
 
-//Criar página "Sobre" = http://localhost:8081/sobre
+//Criar página "Sobre" = http://localhost:5000/sobre
 app.get('/sobre', (req, res)=>{
     res.send('Minha pagina SOBRE')
 })
 
-//Criar página "Blog" = http://localhost:8081/blog
+//Criar página "Blog" = http://localhost:5000/blog
 app.get('/blog', (req, res)=>{
     res.send('Minha pagina BLOG')
 })
 
-//.listen() pede a porta que o servidor vai rodar, deve ser a ultima linha, Acessar navegador e  no link "http://localhost:8081/"
-app.listen(8081, () => {
-    console.log("Servidor rodando na url http://localhost:8081/ ")
+//.listen() pede a porta que o servidor vai rodar, deve ser a ultima linha, Acessar navegador e  no link "http://localhost:5000/"
+app.listen(5000, () => {
+    console.log("Servidor rodando na url http://localhost:5000/ ")
 })  
 // app.listen(porta, função de callback)
--- index.js
+++ index.js
@@ -6,18 +6,18 @@ app.get('/index.html' , (req, res) => {
     res.sendFile(__dirname + "/html/index.html") //__dirname = local absoluto onde a aplicação está rodando
 })
 
-//Criar página "Sobre" = http://localhost:8081/sobre
+//Criar página "Sobre" = http://localhost:5000/sobre
 app.get('/internacional.html', (req, res)=>{
     res.sendFile(__dirname + "/html/internacional.html")
 })
 
-//Criar página "Blog" = http://localhost:8081/blog
+//Criar página "Blog" = http://localhost:5000/blog
 app.get('/blog', (req, res)=>{
     res.send('Minha pagina BLOG')
 })
 
-//.listen() pede a porta que o servidor vai rodar, deve ser a ultima linha, Acessar navegador e  no link "http://localhost:8081/"
-app.listen(8081, () => {
-    console.log("Servidor rodando na url http://localhost:8081/ ")
+//.listen() pede a porta que o servidor vai rodar, deve ser a ultima linha, Acessar navegador e  no link "http://localhost:5000/"
+app.listen(5000, () => {
+    console.log("Servidor rodando na url http://localhost:5000/ ")
 })  
 // app.listen(porta, função de callback)
